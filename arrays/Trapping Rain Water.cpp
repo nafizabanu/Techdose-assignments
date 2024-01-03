1449a1a6ad92d8ffe5c61d203ec24a6bc15d8605
Nafiza Banu <96462117+nafizabanu@users.noreply.github.com>
2024-01-03 12:25:18 +0530

    Add files via upload

---
+++ arrays/Trapping Rain Water.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l=0,r=height.size()-1,leftMax=0,rightMax=0,ans=0;
+        while(l<=r){
+            if(height[l]<=height[r]){
+                if(height[l]>leftMax)leftMax=height[l];
+                else ans+=leftMax-height[l];
+                l++;
+            }else{
+                if(height[r]>rightMax)rightMax=height[r];
+                else ans+=rightMax-height[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+};
